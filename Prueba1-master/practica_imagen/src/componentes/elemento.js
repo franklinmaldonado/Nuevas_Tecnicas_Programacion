8c375368fd9d8a606c4ebc3b45d4f431e8ffd377
Franklin Maldonado <60884549+franklinmaldonado@users.noreply.github.com>
2021-07-15 15:17:57 -0500

    p1

--- Prueba1-master/practica_imagen/src/componentes/elemento.js
+++ Prueba1-master/practica_imagen/src/componentes/elemento.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 class Elemento extends React.Component{
     
-    
+    eliminarElemento=()=>{
+        this.props.eliminarElementoPractica1(this.props.elem.id);
+    }
 
     render(){
         const {elem}= this.props;
         console.log(this.props);
         return <div> 
-        <textarea style= {elem.color} >
+        <textarea style= {elem.color} onClick={this.eliminarElemento}>
             {elem.nombre}
         </textarea>
+        <p>
+         
+        </p>
         </div>
         
     }
