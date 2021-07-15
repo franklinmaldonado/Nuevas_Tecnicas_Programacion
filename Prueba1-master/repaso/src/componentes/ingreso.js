5aa71f3e5f24b8d9291972f8a0fcf1ef5b5175be
Franklin Maldonado <60884549+franklinmaldonado@users.noreply.github.com>
2021-07-14 19:32:34 -0500

    Miercoles

--- Prueba1-master/repaso/src/componentes/ingreso.js
+++ Prueba1-master/repaso/src/componentes/ingreso.js
@@ -3,9 +3,12 @@ import React from 'react'
 class ingreso extends React.Component{
     render(){
 
-        return <h1>
+        return <div>
+           <h1>
             Hola Mundo 
+            <input type ="submit"  onSubmit/>
         </h1>
+        </div>
     }
 }
 export default ingreso
